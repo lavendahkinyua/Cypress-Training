da284c3c9d1c9278203f652dc7ea42f195e78c2c
Lavendah Mwende <lavendah17@gmail.com>
2025-10-13 16:37:39 +0300

    refactored tests

--- cypress/e2e/First_test.cy.js
+++ cypress/e2e/First_test.cy.js
@@ -1,18 +1,23 @@
 describe ("OrangeHrm Testing" , function() {
-  it ("opens sucessfully" , function() {
+  beforeEach(() => {
     cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-    cy.title().should('eq' , 'OrangeHRM')
-    cy.contains('Admin')
-    cy.contains('admin123')
-    cy.get('[name="username"]').type("Admin")
-    cy.get('[name="password"]').type("admin123")
+    cy.get("input[name='username']").type("Admin")
+    cy.get("input[name='password']").type("admin123")
     cy.get('.oxd-button').click()
-    cy.get('.oxd-main-menu-search').should('exist')
-    cy.get('.oxd-input').should('be.enabled')
-    cy.get('.oxd-input').should('have.attr', 'placeholder' ,'Search')
-    cy.get(':nth-child(1) > .oxd-main-menu-item').click()
-    cy.get('nav').should('include.text', 'Qualifications')
-
+ })
+  it ('successful login', function(){
+    cy.contains('Dashboard').should('be.visible')
   })
+
+  it ('search box', function(){
+    cy.get('.oxd-input').as('searchbox')
+    cy.get('@searchbox').should('have.attr','placeholder','Search')
+    cy.get('@searchbox').should('be.enabled')
+  })
+  it ('find user', function(){
+    cy.get('.oxd-main-menu-item').eq(8).click()
+    cy.contains('Rahul Das' , {timeout:40000}).should('be.visible')
+  })
+  
 
 })
